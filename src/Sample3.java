e1d74e0a69016533fa962506ff0cc84ac960394e
Akshay Patil <akshaypatil240988@gmail.com>
2021-02-13 13:27:25 +0530

    code removed

--- src/Sample3.java
+++ src/Sample3.java
@@ -2,10 +2,6 @@ public class Sample3 {
     public static void main(String[] args) {
 
         System.out.println("changes made by Akshay");
-        System.out.println("Changed by Prathmesh");
 
-        System.out.println("23424");
-
-        System.out.println("Final");
     }
 }
